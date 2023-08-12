12cbaf474ce5d32eb6a8dfe50f81bc13a98b59d4
Viraji Sesiri <viraji.sesiri@gmail.com>
2023-08-12 21:13:00 +0530

    Complete Algo-03

--- Algo2.java
+++ Algo2.java
@@ -1,3 +1,15 @@
 public class Algo2 {
+    public static void main(String[] args) {
+        int[] nums = {-5, 2, 7, 10, 58, -7, 8, 23};
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] >= max) max = nums[i];
+            if (nums[i] <= min) min = nums[i];
+            
+        }
+        System.out.println("Maximum number is : " + max);
+        System.out.println("Maximum number is : " + min);
+    }
     
 }
---
+++ Alogo3.java
@@ -0,0 +1,6 @@
+public class Alogo3 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
