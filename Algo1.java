3302f8184cf5b233d84dac22655fbf0f19a8877d
Viraji Sesiri <viraji.sesiri@gmail.com>
2023-08-12 18:58:15 +0530

    Complete Algo-02

---
+++ Algo1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Algo1{
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int number;
+        int sum = 0;
+    loop:
+        do{
+            System.out.print("Enter the number : ");
+            number = scanner.nextInt();
+            scanner.nextLine();
+            if (number < 0) continue loop;
+            
+            
+
+
+
+
+
+
+
+        } while (true);
+    }
+    
+}
