9f877d830d8db11341ace1b34ac5bcdb23e19fdd
Viraji Sesiri <viraji.sesiri@gmail.com>
2023-08-12 22:16:44 +0530

    Complete Algo-04

--- Alogo3.java
+++ Alogo3.java
@@ -1,6 +1,20 @@
 public class Alogo3 {
     public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        int[] newNums = new int[nums.length] ;
+        int count = nums.length -1;
+        for(int i = 0; i < nums.length  ; i++){
+                newNums[i] = nums[count];
+                count--;
+
+            }
+        for (int i = 0; i < nums.length; i++){
+            nums[i] = newNums[i];
+            System.out.println(nums[i]);
+        }
         
+
+        }
     }
     
-}
+
