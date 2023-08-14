fb9de48a24a85680dc10a93a95c5251566f754f8
Viraji Sesiri <viraji.sesiri@gmail.com>
2023-08-14 07:04:54 +0530

    Complete Algo-07

--- Algo7.java
+++ Algo7.java
@@ -1,27 +1,44 @@
-public class Algo7 {
+import java.util.Scanner;
+public class Algo7{
+    private static Scanner scanner = new Scanner (System.in);
     public static void main(String[] args) {
+        String input;
+        String word ="";
+        String smallestWord = "";
+        String LargestWord = "";
+        int length = 0;
 
-        String sen = "We are born to code";
+        do{
+            System.out.print("Enter the sentence : ");
+            input = scanner.nextLine().strip();
 
-        String[]  words = sen.split("");
-        String count;
+            String[] words = input.split("\\s");
 
-        for (int i =0; i < words.length; i ++){
-           // System.out.print(words[i]);
-           count = words[i];
-           words[i] = words[words.length - i -1];
-           words[words.length - i -1] = count;
-           System.out.print(count);
+            input = input + "";
 
+            for (int i = 0; i < input.length(); i++){
+                if (input.charAt(i) != ' '){
+                    word += input.charAt(i);
+                }else{
+                    words[length] = word;
+                    length++;
+                    word = ""; 
+                }
+            }
+            smallestWord = words[0];
+            LargestWord = words[0];
 
-        }
-        for (int i = 0; i < words.length; i++){
-           // System.out.print(words[i]);
+            for (int k = 0; k < length; k++){
+                if(smallestWord.length() > words[k].length()) smallestWord = words[k];
+                //if(smallestWord.length() == words[k].length()) smallestWord += "" +words[k];
+                if(LargestWord.length() < words[k].length()) LargestWord = words[k];
+            }
+
+            System.out.printf("Smallest Word ---> %s - %s \n",smallestWord , smallestWord.length());
+            System.out.printf("Largest Word ---> %s - %s \n",LargestWord , LargestWord.length());
+
             
-
-        }
-        System.out.println();
-        
+        }while (input.isBlank());
     }
-    
+
 }
